ee79e8f243a0437a4dbb9be19fb60c6dd0c82082
Markus Gustavsson <markus.gustavsson@gmail.com>
2013-02-11 14:57:10 +0100

    WIP: object protocol

--- src/oct_object.c
+++ src/oct_object.c
@@ -1,2 +1,21 @@
 #include "oct_object.h"
+#include "oct_context.h"
+#include "oct_runtime.h"
 
+oct_Bool _oct_Object_initType(struct oct_Context* ctx) {
+}
+
+oct_Bool oct_Object_symbolp(struct oct_Context* ctx, oct_Object obj, oct_Bool* out_bool) {
+	*out_bool = ctx->rt->builtInTypes.Symbol == obj.vtable->instanceType.ptr;
+	return oct_True;
+}
+
+oct_Bool oct_Object_stringp(struct oct_Context* ctx, oct_Object obj, oct_Bool* out_bool) {
+	*out_bool = ctx->rt->builtInTypes.String == obj.vtable->instanceType.ptr;
+	return oct_True;
+}
+
+oct_Bool oct_Object_listp(struct oct_Context* ctx, oct_Object obj, oct_Bool* out_bool) {
+	*out_bool = ctx->rt->builtInTypes.List == obj.vtable->instanceType.ptr;
+	return oct_True;
+}
--- src/oct_object.h
+++ src/oct_object.h
@@ -6,33 +6,63 @@
 
 #include "oct_primitives.h"
 #include "oct_type_pointers.h"
-#include "oct_list.h"
+#include "oct_nothing.h"
 
 // Private
 
 struct oct_Context;
 
-oct_Bool _oct_Object_initType(struct oct_Context* ctx);
+oct_Bool _oct_BObject_initType(struct oct_Context* ctx);
+oct_Bool _oct_OObject_initType(struct oct_Context* ctx);
+oct_Bool _oct_MObject_initType(struct oct_Context* ctx);
+oct_Bool _oct_OObjectOption_initType(struct oct_Context* ctx);
 
 // Public
 
 struct oct_ObjectVTable;
 
 typedef struct oct_Object {
-	void* object; // Borrowed?
+	void* object;
 	struct oct_ObjectVTable* vtable;
 } oct_Object;
 
+typedef struct oct_BObject {
+	oct_Object object;
+} oct_BObject;
+
+typedef struct oct_OObject {
+	oct_Object object;
+} oct_OObject;
+
+typedef struct oct_MObject {
+	oct_Object object;
+} oct_MObject;
+
+#define OCT_OOBJECTOPTION_NOTHING 0
+#define OCT_OOBJECTOPTION_OBJECT 1
+
+typedef struct oct_OObjectOption {
+	oct_Uword variant;
+	union {
+		oct_Nothing nothing;
+		oct_OObject object;
+	};
+} oct_OObjectOption;
+
 typedef struct oct_ObjectVTable {
 	oct_BType instanceType;
-	oct_Bool(*ctor)       (struct oct_Context* ctx, void* object, oct_OList args);
+	//oct_Bool(*ctor)       (struct oct_Context* ctx, void* object, oct_OList args);
 	oct_Bool(*dtor)       (struct oct_Context* ctx, void* object);
 	//oct_Bool(*print)      (struct oct_Context* ctx, void* object, /*Text output stream*/);
-	oct_Bool(*invoke)     (struct oct_Context* ctx, void* object, oct_OList args);
+	//oct_Bool(*invoke)     (struct oct_Context* ctx, void* object, oct_OList args);
 	oct_Bool(*copyOwned)  (struct oct_Context* ctx, void* object, void** out_copy);
 	oct_Bool(*copyManaged)(struct oct_Context* ctx, void* object, void** out_copy);
 	oct_Bool(*hash)       (struct oct_Context* ctx, void* object, oct_Uword* out_hash);
-	oct_Bool(*equals)     (struct oct_Context* ctx, void* object, oct_Object other);
+	oct_Bool(*equals)     (struct oct_Context* ctx, void* object, oct_BObject other);
 } oct_ObjectVTable;
+
+oct_Bool oct_Object_symbolp(struct oct_Context* ctx, oct_BObject obj, oct_Bool* out_bool);
+oct_Bool oct_Object_stringp(struct oct_Context* ctx, oct_BObject obj, oct_Bool* out_bool);
+oct_Bool oct_Object_listp(struct oct_Context* ctx, oct_BObject obj, oct_Bool* out_bool);
 
 #endif
