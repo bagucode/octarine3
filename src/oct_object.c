2a133f6995824c1efcda5277cd94413e679a4d38
Bagu is my name. Show my code to River Man <markus.gustavsson@gmail.com>
2013-02-12 20:13:34 +0100

    ok

--- src/oct_object.c
+++ src/oct_object.c
@@ -6,17 +6,17 @@
 oct_Bool _oct_Object_initType(struct oct_Context* ctx) {
 }
 
-oct_Bool oct_Object_symbolp(struct oct_Context* ctx, oct_Object obj, oct_Bool* out_bool) {
-	*out_bool = ctx->rt->builtInTypes.Symbol == obj.vtable->instanceType.ptr;
+oct_Bool oct_Object_symbolp(struct oct_Context* ctx, oct_BObject obj, oct_Bool* out_bool) {
+	*out_bool = ctx->rt->builtInTypes.Symbol == obj.object.vtable->instanceType.ptr;
 	return oct_True;
 }
 
-oct_Bool oct_Object_stringp(struct oct_Context* ctx, oct_Object obj, oct_Bool* out_bool) {
-	*out_bool = ctx->rt->builtInTypes.String == obj.vtable->instanceType.ptr;
+oct_Bool oct_Object_stringp(struct oct_Context* ctx, oct_BObject obj, oct_Bool* out_bool) {
+	*out_bool = ctx->rt->builtInTypes.String == obj.object.vtable->instanceType.ptr;
 	return oct_True;
 }
 
-oct_Bool oct_Object_listp(struct oct_Context* ctx, oct_Object obj, oct_Bool* out_bool) {
-	*out_bool = ctx->rt->builtInTypes.List == obj.vtable->instanceType.ptr;
+oct_Bool oct_Object_listp(struct oct_Context* ctx, oct_BObject obj, oct_Bool* out_bool) {
+	*out_bool = ctx->rt->builtInTypes.List == obj.object.vtable->instanceType.ptr;
 	return oct_True;
 }
