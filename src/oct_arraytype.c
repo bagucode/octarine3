fb79db60231ee86b11195f21b22c4ea51f502708
Markus Gustavsson <markus.gustavsson@gmail.com>
2013-02-27 17:06:49 +0100

    speling mistake

--- src/oct_arraytype.c
+++ src/oct_arraytype.c
@@ -8,7 +8,7 @@
 
 #define CHECK(X) if(!X) return oct_False;
 
-oct_Bool _oct_Arra_init(struct oct_Context* ctx) {
+oct_Bool _oct_Array_init(struct oct_Context* ctx) {
 	oct_BType t = ctx->rt->builtInTypes.Array;
 
 	// Array
